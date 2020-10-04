0a2d28ad77ad396047a4eebdf031ae85bc64fb65
Yaniv Kremer <yanivkr7@gmail.com>
2020-10-04 22:48:17 +0300

    Updates

---
+++ precache-manifest.96ba802ab776ccd64680ac8659b60eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228ae88d3475d1ce7994896f4f8303c8",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "7429c62067999781df49",
+    "url": "/monsters/static/css/main.0ebca6de.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7429c62067999781df49",
+    "url": "/monsters/static/js/main.b60c172f.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
