b9066d85c8c1fd88fe9652059ac8018fa565e04d
Yaniv Kremer <yanivkr7@gmail.com>
2020-10-04 22:52:26 +0300

    Search just by name

--- src/App.js
+++ src/App.js
@@ -23,10 +23,8 @@ class App extends Component {
 
 	render() {
 		const { monsters, searchField } = this.state;
-		const filteredMonsters = monsters.filter(
-			(monster) =>
-				monster.name.toLowerCase().includes(searchField.toLowerCase()) ||
-				monster.email.toLowerCase().includes(searchField.toLowerCase())
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
 		);
 
 		return (
